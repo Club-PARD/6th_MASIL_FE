4d236ed68bc052132ef1011cdc4ef472b9bed1cd
Yoomin Hwang <yoominh3@gmail.com>
2025-08-16 21:01:30 +0900

    임시 커밋

--- src/components/GuideCard.tsx
+++ src/components/GuideCard.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState } from "react";
 
 export interface CardProps extends HTMLAttributes<HTMLDivElement> {
     number: number; // 몇 번째 코스인지
@@ -11,21 +11,24 @@ const Card = ({
     course,
     ...props
     }: CardProps) => {
+    
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="w-[1000px] h-24 flex flex-row items-center justify-around bg-neutral-100 rounded-2xl">
-            <div className="flex flex-col items-center justify-center">
-                <span className="text-3xl text-neutral-900">코스 {number}</span>
+        <div className="flex flex-row w-[1121px] h-[130px] px-[72px] py-[29px] bg-white rounded-[20px] items-center justify-between">
+            <div>
+                <span className="text-3xl">코스 {number}</span>
             </div>
             <div className="flex flex-row space-x-2">
                 {course.map((location, index) => (
-                    <div key={index} className="flex flex-row items-center text-2xl space-x-2">
+                    <div key={index} className="text-2xl space-x-2">
                         <span>{location}</span>
                         {index !== course.length - 1 && <span>→</span>}
                     </div>
                 ))}
             </div>
-            <div>
-                <a href="#" className="underline">자세히 보기</a>
+            <div className="px-[15px] py-2.5 bg-neutral-100 rounded-[5px] gap-2.5 cursor-pointe text-xl font-semibold" onClick={() => setOpen(!open)}>
+                상세 보기
             </div>
         </div>
     );
